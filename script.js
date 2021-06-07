a9ca419a8e7be578696378a74bc2b60e8ef3a0b2
sindetisarah <sindetisara@gmail.com>
2021-06-07 19:23:13 +0300

    added an explanation fro the carousel

--- script.js
+++ script.js
@@ -16,11 +16,25 @@ function carousel() {
   setTimeout(carousel, 3000);
 }
 
+//This is how the scripting of corousel qworks in my own words:
+//BDD
+// In this case we have defined the function and invoked it .
+// We then hoist our variable i not to limit the scope of the variable
+// Remember in the corousel we had different images or rather more than one image .
+// We threfore loop through them at different intervals of 3000 milliseconds and display them
+
+//In simple term here we have a call back function of which it is the set timeout.
+// The setimeout here registers the function carousel which is to be invoked after 3000 milliseconds
+//With our most important methods like getElementByClassName we access the different sets of pictures and display them in a slide show
+// //created a function
 function Book(){
+  //fetching the elements
   var company_name = document.getElementById("company_name").value;
   var phone_number = document.getElementById("phone_number").value;
   var location = document.getElementById("location").value;
-  alert(`${company_name},${phone_number},${location}`)
+  var date = document.getElementById("date").value;
+  var time=document.getElementById("time").value;
+  alert(`Registered your company${company_name}, and phone number${phone_number}, location${location} booking date${date} and booking time${time}`)
 }
 
 
